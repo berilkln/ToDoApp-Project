28b36a9353ba756007d585df230797f2c783d1fa
Beril Kartal <berilk.dev@gmail.com>
2024-11-25 00:12:48 +0300

    fix: Edit password reset urls

--- ToDoApp/accounts/urls.py
+++ ToDoApp/accounts/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('accounts/register/', account_views.register, name='register'),
     path('accounts/login/', account_views.user_login, name='login'),
     path('accounts/logout/', account_views.user_logout, name='logout'),
-    path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset/', PasswordResetView.as_view(template_name='accounts/password_reset.html'), name='password_reset'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
     path('profile/', account_views.profile, name='profile'),
 ]
