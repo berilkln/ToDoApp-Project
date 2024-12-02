376c2a6dab9df7224b5c9a55e0aa25529afe97c0
Beril Kartal <berilk.dev@gmail.com>
2024-12-02 20:57:26 +0300

    Fix syntax error in settings.py

--- ToDoApp/ToDoApp/settings.py
+++ ToDoApp/ToDoApp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
+    'accounts',
     'dashboard'
 ]
 
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
